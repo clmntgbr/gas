1f52f39bc8ee82ada5b675ad46c2b6157976b260
clement <clement.goubier@display-interactive.com>
2022-05-12 08:29:32 +0200

    removing service

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Service\RabbitMQService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,9 +10,8 @@ use Symfony\Component\Routing\Annotation\Route;
 class AppController extends AbstractController
 {
     #[Route('/test', name: 'test')]
-    public function test(RabbitMQService $rabbitMQService, Request $request): JsonResponse
+    public function test(Request $request): JsonResponse
     {
-        $rabbitMQService->getQueues();
         return JsonResponse::fromJsonString('{"test": "test"}');
     }
 }
